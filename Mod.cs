aaf59170948231a1a73d5017e2fe27e510327641
BruceZoom <wangzhongye1110@qq.com>
2023-08-11 20:01:15 -0400

    bugfix, implement pinned only and reset button

--- Mod.cs
+++ Mod.cs
@@ -20,7 +20,10 @@ namespace BetterSideBarNS
         // MARK: choose to persist names of favorite ideas
         // because ideaElements may change between different runs
         private static ConfigEntry<string> __fideas;
-        public static List<string> FIdeas;
+        public static List<string> Fideas;
+
+        public static ConfigEntry<bool> HideUnfavor;
+
         // array recording wether each idea is favorite
         // same shape as ideaElements
         private static bool[] isFidea;
@@ -30,10 +33,17 @@ namespace BetterSideBarNS
         private static Dictionary<BlueprintGroup, int> groupFNumMap;
         // the index (in UI) of the first element in a group
         private static Dictionary<BlueprintGroup, int> groupUIIdxMap;
-        // the ordered list of all group names
+        
         private static List<BlueprintGroup> groups;
+        private static List<IdeaElement> ideaElements;
 
         public static Sprite FavorIcon;
+
+        private static CustomButton hideUnfavorBtn;
+        private static CustomButton resetFavorBtn;
+
+        // callback for reseting favorite ideas
+        public static event Action ResetFavor;
 
         private void Awake()
         {
@@ -47,9 +57,12 @@ namespace BetterSideBarNS
             C = Config;
             // initialize favorite list
             __fideas = Config.GetEntry<string>("favorite_ideas", "");
-            //__fideas.UI.Hidden = true;
-            FIdeas = __fideas.Value.Split(FileSeparator).ToList();
-            FIdeas.Remove("");
+            __fideas.UI.Hidden = true;
+            Fideas = __fideas.Value.Split(FileSeparator).ToList();
+            Fideas.Remove("");
+
+            // initialize hide unfavor toggle
+            HideUnfavor = Config.GetEntry<bool>("hide_unfavor", false);
 
             groupIdxMap = new Dictionary<BlueprintGroup, int>();
             groupFNumMap = new Dictionary<BlueprintGroup, int>();
@@ -68,8 +81,7 @@ namespace BetterSideBarNS
         }
 
         /// <summary>
-        /// Set up the scope to initialize idea elemets 
-        /// and initial re-ordering of idea elements
+        /// Initialize idea elements with the favoring functionality
         /// </summary>
         [HarmonyPatch(typeof(GameScreen), "InitIdeaElements")]
         public class FavoriteButtonAssitHarmonyPatches
@@ -87,21 +99,22 @@ namespace BetterSideBarNS
                     groupUIIdxMap.Add(group, ___ideaLabels[i].transform.GetSiblingIndex() + 1);
                 }
                 // initialize isFidea
+                ideaElements = ___ideaElements;
                 isFidea = new bool[___ideaElements.Count];
-                int[] initFideasIdx = new int[FIdeas.Count];
+                int[] initFideasIdx = new int[Fideas.Count];
 
                 // Initialize each idea element with the favorite functionality
                 for (int i=0; i<___ideaElements.Count; i++)
                 {
                     IdeaElement ideaElement = ___ideaElements[i];
                     string cardId = ideaElement.MyKnowledge.CardId;
-                    int idx_fi = FIdeas.IndexOf(cardId);
+                    int idx_fi = Fideas.IndexOf(cardId);
                     // MARK: need to create a new local variable idx_ie
                     // otherwise, i is a global variable w.r.t. to delegate below
                     // i cannot faithfully reflect the true index of the element
                     int idx_ie = i;
 
-                    isFidea[idx_ie] = FIdeas.Contains(cardId);
+                    isFidea[idx_ie] = Fideas.Contains(cardId);
                     groupIdxMap[ideaElement.MyKnowledge.Group] += 1;
 
                     // initialize favor icon
@@ -114,27 +127,55 @@ namespace BetterSideBarNS
                     ideaElement.gameObject.GetComponent<CustomButton>().Clicked += delegate
                     {
                         // remove from favorites
-                        L.Log(idx_ie.ToString());
                         if (isFidea[idx_ie])
                         {
-                            L.Log("Remove " + cardId);
+                            //L.Log("Remove " + cardId);
                             isFidea[idx_ie] = false;
-                            FIdeas.Remove(cardId);
-                            UnpinIdea(idx_ie, ideaElement);
+                            Fideas.Remove(cardId);
+                            groupFNumMap[ideaElement.MyKnowledge.Group] -= 1;
+                            // this unpin should happen only when the idea is unhovered
+                            HideUnhoveredCoroutine.StartCoroutine(idx_ie, delegate
+                            {
+                                UnpinIdea(idx_ie, ideaElement);
+                                if (HideUnfavor.Value)
+                                {
+                                    ideaElement.gameObject.SetActive(false);
+                                }
+                            });
                             favorLabel.SetActive(false);
                         }
                         // add to favorites
                         else
                         {
-                            L.Log("Add " + cardId);
+                            // can only interrupt the hiding coroutine
+                            // when another pin update happens
+                            HideUnhoveredCoroutine.InterruptCoroutine();
+
+                            //L.Log("Add " + cardId);
                             isFidea[idx_ie] = true;
-                            FIdeas.Add(cardId);
+                            Fideas.Add(cardId);
+                            groupFNumMap[ideaElement.MyKnowledge.Group] += 1;
                             PinIdea(idx_ie, ideaElement);
                             favorLabel.SetActive(true);
                         }
-                        // update persisted data
-                        __fideas.Value = string.Join(FileSeparator, FIdeas);
-                        C.Save();
+                        // save
+                        SaveConfig();
+                        // update UI
+                        UpdateIdea(idx_ie);
+                        //GameScreen.instance.UpdateIdeasLog();
+                    };
+
+                    ResetFavor += delegate
+                    {
+                        if (isFidea[idx_ie])
+                        {
+                            L.Log("Remove " + cardId);
+                            isFidea[idx_ie] = false;
+                            Fideas.Remove(cardId);
+                            groupFNumMap[ideaElement.MyKnowledge.Group] -= 1;
+                            UnpinIdea(idx_ie, ideaElement);
+                            favorLabel.SetActive(false);
+                        }
                     };
 
                     // record idea elements need re-ordering
@@ -155,20 +196,24 @@ namespace BetterSideBarNS
                 // initial reodering
                 foreach (int idx_ie in initFideasIdx)
                 {
+                    groupFNumMap[___ideaElements[idx_ie].MyKnowledge.Group] += 1;
                     PinIdea(idx_ie, ___ideaElements[idx_ie]);
                 }
             }
         }
 
-        // pin an idea and increase favorite number of the group
+        /// <summary>
+        /// pin an idea and increase favorite number of the group
+        /// </summary>
         static void PinIdea(int idx_ie, IdeaElement ie)
         {
             BlueprintGroup group = ie.MyKnowledge.Group;
-            ie.transform.SetSiblingIndex(groupUIIdxMap[group] + groupFNumMap[group]);
-            groupFNumMap[group] += 1;
+            ie.transform.SetSiblingIndex(groupUIIdxMap[group] + groupFNumMap[group]-1);
         }
 
-        // unpin an idea and increase favorite number of the group
+        /// <summary>
+        /// unpin an idea and increase favorite number of the group
+        /// </summary>
         static void UnpinIdea(int idx_ie, IdeaElement ie)
         {
             BlueprintGroup group = ie.MyKnowledge.Group;
@@ -179,8 +224,224 @@ namespace BetterSideBarNS
                 // is not included in pinned elements
                 offset += (isFidea[i] ? 0 : 1);
             }
-            groupFNumMap[group] -= 1;
             ie.transform.SetSiblingIndex(groupUIIdxMap[group] + groupFNumMap[group] + offset);
+        }
+
+        /// <summary>
+        /// Add buttons for hiding unfavorite ideas and reseting favorite ideas
+        /// </summary>
+        [HarmonyPatch(typeof(GameScreen), "Awake")]
+        public class ExtraButtonHarmonyPatches
+        {
+            public static void Postfix()
+            {
+                // credit to https://github.com/Jakhes/extra_search_mod
+                GameObject extraBtnParentObj = new GameObject();
+                HorizontalLayoutGroup extraBtnParentLayout = extraBtnParentObj.AddComponent<HorizontalLayoutGroup>();
+                extraBtnParentLayout.childForceExpandHeight = false;
+                extraBtnParentLayout.childForceExpandWidth = false;
+                extraBtnParentLayout.childControlHeight = true;
+                extraBtnParentLayout.childControlWidth = true;
+                extraBtnParentLayout.spacing = 10;
+                
+                extraBtnParentObj.transform.SetParent(GameScreen.instance.IdeaSearchField.transform.parent.parent);
+                extraBtnParentObj.transform.SetSiblingIndex(1);
+
+                // hide unfavorite idea button
+                GameObject hideUnfavorBtnObj = GameObject.Instantiate(GameScreen.instance.IdeasButton.gameObject);
+                hideUnfavorBtnObj.AddComponent<LayoutElement>();
+                hideUnfavorBtnObj.GetComponent<LayoutElement>().minWidth = 220;
+                hideUnfavorBtnObj.GetComponent<LayoutElement>().preferredWidth = 220;
+                hideUnfavorBtnObj.transform.SetParent(extraBtnParentObj.transform);
+
+                hideUnfavorBtn = hideUnfavorBtnObj.GetComponent<CustomButton>();
+                hideUnfavorBtn.TextMeshPro.fontSize = 25;
+                hideUnfavorBtn.TooltipText = "Only display pinned ideas and new ideas.";
+                hideUnfavorBtn.Clicked += delegate
+                {
+                    // toggle HideUnfavor boolean
+                    HideUnfavor.Value = !HideUnfavor.Value;
+                    // update UI
+                    GameScreen.instance.UpdateIdeasLog();
+                    // save
+                    SaveConfig();
+                };
+
+                // reset favorite idea button
+                GameObject resetFavorBtnObj = GameObject.Instantiate(GameScreen.instance.IdeasButton.gameObject);
+                resetFavorBtnObj.AddComponent<LayoutElement>();
+                resetFavorBtnObj.GetComponent<LayoutElement>().minWidth = 100;
+                resetFavorBtnObj.GetComponent<LayoutElement>().preferredWidth = 100;
+                resetFavorBtnObj.transform.SetParent(extraBtnParentObj.transform);
+
+                resetFavorBtn = resetFavorBtnObj.GetComponent<CustomButton>();
+                resetFavorBtn.TextMeshPro.fontSize = 25;
+                resetFavorBtn.TooltipText = "Reset favorite ideas.";
+                resetFavorBtn.Clicked += delegate
+                {
+                    // reset HideUnfavor boolean
+                    HideUnfavor.Value = false;
+                    // reset favorite ideas
+                    ResetFavor?.Invoke();
+                    // update UI
+                    GameScreen.instance.UpdateIdeasLog();
+                    // save
+                    SaveConfig();
+                };
+            }
+        }
+
+        /// <summary>
+        /// Renders content for extra buttons
+        /// can only be done later than Awake or else the text doesnt change
+        /// </summary>
+        [HarmonyPatch(typeof(GameScreen), "Update")]
+        public class RenderExtraButtonHarmonyPatches
+        {
+            public static void Prefix()
+            {
+                hideUnfavorBtn.TextMeshPro.text = "Pinned Only";
+                hideUnfavorBtn.Image.color = (HideUnfavor.Value ?
+                                                ColorManager.instance.ButtonColor :
+                                                ColorManager.instance.DisabledColor);
+                hideUnfavorBtn.TextMeshPro.color = (HideUnfavor.Value ?
+                                                    ColorManager.instance.ButtonTextColor :
+                                                    ColorManager.instance.DisabledButtonTextColor);
+
+                resetFavorBtn.TextMeshPro.text = "Reset";
+                resetFavorBtn.Image.color = ColorManager.instance.ButtonColor;
+                resetFavorBtn.TextMeshPro.color = ColorManager.instance.ButtonTextColor;
+            }
+        }
+
+        [HarmonyPatch(typeof(GameScreen), "UpdateIdeasLog")]
+        public class UpdateIdeasLogHarmonyPatches
+        {
+            public static void Postfix()
+            {
+                for (int i=0; i<ideaElements.Count; i++)
+                {
+                    UpdateIdea(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// In the Pinned Only mode, additional checking and updates are required
+        /// to keep new ideas and just unfavored ideas visible
+        /// </summary>
+        static void UpdateIdea(int idx_ie)
+        {
+            IdeaElement element = ideaElements[idx_ie];
+            // element's activity differs from the default only when HideUnfavor is true
+            if (HideUnfavor.Value)
+            {
+                // element is active only when
+                // - it is favorite
+                // - it is new
+                // - it is hovered or clicked
+                // TODO: this could be implemented better if there is a way to add
+                // an oneshot callback to the element or its button's update
+                element.gameObject.SetActive(isFidea[idx_ie] || element.IsNew ||
+                    element.MyButton.IsHovered || element.MyButton.IsSelected);
+                // when the element is active even when it is not favorite,
+                // it is then displayed due to other conditions,
+                // need a coroutine to wait until other conditons are no longer valid and hide it
+                if ((element.MyButton.IsHovered || element.MyButton.IsSelected || element.IsNew)
+                    && !isFidea[idx_ie])
+                {
+                    HideUnhoveredCoroutine.StartCoroutine(idx_ie, delegate {
+                        element.gameObject.SetActive(false);
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Encapsulation for HideUnhoveredCoroutine methods
+        /// </summary>
+        public static class HideUnhoveredCoroutine
+        {
+            public static int hidingUnhoveredIdeaIdx;
+            public static event Action hidingUnhoveredIdeaCallback;
+
+            public static void InterruptCoroutine()
+            {
+                hidingUnhoveredIdeaIdx = -1;
+            }
+
+            public static void StartCoroutine(int idx_ie, Action callback)
+            {
+                // prevent reentry
+                if (hidingUnhoveredIdeaIdx != -1) return;
+                hidingUnhoveredIdeaIdx = idx_ie;
+                hidingUnhoveredIdeaCallback = callback;
+            }
+
+            public static void StartCoroutine(int idx_ie)
+            {
+                // prevent reentry
+                if (hidingUnhoveredIdeaIdx != -1) return;
+                hidingUnhoveredIdeaIdx = idx_ie;
+                hidingUnhoveredIdeaCallback = null;
+            }
+
+            public static void InvokeCallback()
+            {
+                hidingUnhoveredIdeaCallback?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Running the condition checker for HideUnhoveredCoroutine
+        /// </summary>
+        [HarmonyPatch(typeof(GameScreen), "Update")]
+        public class HideUnhoveredCoroutineHarmonyPatches
+        {
+            public static void Postfix()
+            {
+                // when a valid hovered idea is waiting to hide,
+                // and it is no longer hovered/selected/new, then hide it
+                int idx = HideUnhoveredCoroutine.hidingUnhoveredIdeaIdx;
+                if (idx > -1 && !ideaElements[idx].MyButton.IsHovered &&
+                    !ideaElements[idx].MyButton.IsSelected && !ideaElements[idx].IsNew)
+                {
+                    HideUnhoveredCoroutine.hidingUnhoveredIdeaIdx = -1;
+                    HideUnhoveredCoroutine.InvokeCallback();
+                }
+            }
+        }
+
+        /*
+        static IEnumerator HideUnhoveredIdea(int idx_ie)
+        {
+            IdeaElement element = ideaElements[idx_ie];
+            hidingUnhoveredIdea = true;
+            // exit when another update interrupt the coroutine
+            while (hidingUnhoveredIdea)
+            {
+                // continue looping when it is still hovered and exit otherwise
+                if (element.MyButton.IsHovered || element.MyButton.IsSelected)
+                {
+                    yield return null;
+                }
+                else
+                {
+                    element.gameObject.SetActive(false);
+                    break;
+                }
+            }
+        }*/
+
+        /// <summary>
+        /// Save configuration
+        /// </summary>
+        public static void SaveConfig()
+        {
+            // store favorites
+            __fideas.Value = string.Join(FileSeparator, Fideas);
+            // save
+            C.Save();
         }
     }
 }
